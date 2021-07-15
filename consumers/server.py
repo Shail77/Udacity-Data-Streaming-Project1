b6b57e992cd105d36892b3e3d91ddf126e8d4146
Shail77 <87279035+Shail77@users.noreply.github.com>
2021-07-15 20:36:55 +0530

    Update server.py
    made topics same as producers topic

--- consumers/server.py
+++ consumers/server.py
@@ -68,18 +68,12 @@ def run_server():
             offset_earliest=True,
         ),
         KafkaConsumer(
-            "org.chicago.cta.stations.table.v1",
-            lines.process_message,
-            offset_earliest=True,
-            is_avro=False,
-        ),
-        KafkaConsumer(
             "^org.chicago.cta.station.arrivals.",
             lines.process_message,
             offset_earliest=True,
         ),
         KafkaConsumer(
-            "TURNSTILE_SUMMARY",
+            "org.chicago.cta.turnstile",
             lines.process_message,
             offset_earliest=True,
             is_avro=False,
